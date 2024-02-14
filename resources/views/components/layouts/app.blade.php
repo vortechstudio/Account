122fd6b81eca925153db7e68b643d2392ff0c686
Vortechstudio <contact@vortechstudio.fr>
2024-02-14 20:01:22 +0100

    fix(app.blade.php): Supprimer le code obsolète de configuration de thème
    - Supprimer le code obsolète de configuration de thème dans le fichier app.blade.php pour améliorer la lisibilité et la maintenance du code.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,27 +10,6 @@
         <link href="{{ asset('/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
     </head>
     <body id="kt_app_body" data-kt-app-header-fixed-mobile="true" data-kt-app-toolbar-enabled="true" class="app-default">
-    <!--begin::Theme mode setup on page load-->
-    <script>
-        let defaultThemeMode = "light";
-        let themeMode;
-        if (document.documentElement) {
-            if (document.documentElement.hasAttribute("data-bs-theme-mode")) {
-                themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
-            } else {
-                if (localStorage.getItem("data-bs-theme") !== null) {
-                    themeMode = localStorage.getItem("data-bs-theme");
-                } else {
-                    themeMode = defaultThemeMode;
-                }
-            }
-            if (themeMode === "system") {
-                themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-            }
-            document.documentElement.setAttribute("data-bs-theme", themeMode);
-        }
-    </script>
-    <!--end::Theme mode setup on page load-->
     <!--begin::App-->
     <div class="d-flex flex-column flex-root app-root" id="kt_app_root">
         <!--begin::Page-->
