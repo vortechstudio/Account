328bf531cc667acc4b574701c022076687a04910
vortechstudio <vortechstudio@users.noreply.github.com>
2024-02-15 20:06:11 +0000

    Fix styling

--- app/Livewire/Account/LoginHistory.php
+++ app/Livewire/Account/LoginHistory.php
@@ -11,8 +11,10 @@ use Livewire\WithPagination;
 
 class LoginHistory extends Component
 {
-    use WithPagination, LivewireAlert;
+    use LivewireAlert, WithPagination;
+
     public string $orderField = 'login_at';
+
     public string $orderDirection = 'ASC';
 
     protected $queryString = [
@@ -20,7 +22,8 @@ class LoginHistory extends Component
         'orderDirection' => ['except' => 'ASC'],
     ];
 
-    public function setOrderField(string $name) {
+    public function setOrderField(string $name)
+    {
         if ($name === $this->orderField) {
             $this->orderDirection = $this->orderDirection === 'ASC' ? 'DESC' : 'ASC';
         } else {
@@ -34,26 +37,26 @@ class LoginHistory extends Component
         try {
             Device::query()->find($id)
                 ->delete();
-            $this->alert("success", "Appareille déconnecter avec succès");
-        }catch (\Exception $exception) {
+            $this->alert('success', 'Appareille déconnecter avec succès');
+        } catch (\Exception $exception) {
             \Log::emergency($exception->getMessage(), [$exception]);
-            $this->alert("error", "Erreur lors du traitement");
+            $this->alert('error', 'Erreur lors du traitement');
         }
     }
 
-    #[Title("Historique de connexion")]
+    #[Title('Historique de connexion')]
     public function render()
     {
         $tracking = new LaravelDeviceTracking();
 
-        return view('livewire.account.login-history',[
-            "logs" => auth()->user()->authentications()
+        return view('livewire.account.login-history', [
+            'logs' => auth()->user()->authentications()
                 ->orderBy($this->orderField, $this->orderDirection)
                 ->paginate(5),
-            "actual_login" => $tracking->findCurrentDevice(),
-            "devices" => auth()->user()->device()->get()
+            'actual_login' => $tracking->findCurrentDevice(),
+            'devices' => auth()->user()->device()->get(),
 
         ])
-            ->layout("components.layouts.app");
+            ->layout('components.layouts.app');
     }
 }
